dc95d16cf57aba69b6dcfe1d30a98b6856eed357
alexesprit <alex.esprit@gmail.com>
2020-11-21 22:14:16 +0300

    Update plugin
    Move to `html-validate` v4 API.
    
    Split processing files and displaying reports:
    - Remove throwing errors in the main function.
    - Add `format` and `failAfterError` functions which can be used in pipeline.
    
    This way allows to print `html-validate` results for every file and (optionally) fail on errors after *all* files are processed.

--- README.md
+++ README.md
@@ -14,27 +14,44 @@ Validate files with `html-validate`.
 const gulp = require('gulp');
 const htmlvalidate = require('gulp-html-validate');
 
-exports.default = () => gulp.src('index.html').pipe(htmlvalidate());
-```
-
-You can also pass options to the `htmlvalidate` function:
-
-```js
-exports.default = () =>
-	gulp.src('index.html').pipe(
-		htmlvalidate({
-			/* Your options here */
-		})
+exports.default = () => {
+	return (
+		gulp
+			.src('index.html')
+			/*
+			 * Aaply the `html-validate` report to each file object, so these
+			 * reports can be used by other modules.
+			 */
+			.pipe(htmlvalidate())
+			/*
+			 * Output `html-validate` results to the console.
+			 */
+			.pipe(htmlvalidate.format())
+			/*
+			 * Make gulp to exit with an error code if any error(s) occurred.
+			 */
+			.pipe(htmlvalidate.failAfterError())
 	);
+};
 ```
 
-### Options
+## API
 
-#### format
+### htmlvalidate()
 
-Any formatter supported by `html-validate` (`stylish` by default).
+Apply html-validate report to each file.
 
-Type: `string`.
+### format(formatter)
+
+Print a report where all errors/warnings are listed. This function should be used in the stream after piping through `htmlvalidate` function; otherwise, it will find no `html-validate` results to format.
+
+The `formatter` parameter is a string, which defined the formatter name to use.
+
+Any formatter supported by `html-validate` can be used. The `stylish` formatter is used by default.
+
+### failAfterError()
+
+Throw an error if `html-validate` found an error in any file. All files will be processed before throwing an error.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+const fancyLog = require('fancy-log');
 const through = require('through2');
 const PluginError = require('plugin-error');
 
-const { HtmlValidate } = require('html-validate');
-const { getFormatter } = require('html-validate/build/cli/formatter');
+const { Reporter, CLI } = require('html-validate');
 
 const pluginName = 'gulp-html-validate';
-const defaultOptions = {
-	format: 'stylish',
-};
+const defaultFormatter = 'stylish';
 
-module.exports = (options) => {
-	const pluginOptions = options || defaultOptions;
+const htmlValidateCli = new CLI();
 
-	const htmlvalidate = new HtmlValidate();
-	const formatter = getFormatter(pluginOptions.format);
+module.exports = gulpHtmlValidate;
 
-	return through.obj(function(file, encoding, callback) {
+/**
+ * Apply html-validate report to each file.
+ *
+ * @return {Object} Stream object
+ */
+function gulpHtmlValidate() {
+	const htmlvalidate = htmlValidateCli.getValidator();
+
+	return through.obj((file, encoding, callback) => {
 		if (file.isNull()) {
 			callback(null, file);
 			return;
@@ -26,20 +30,103 @@ module.exports = (options) => {
 			return;
 		}
 
-		const result = htmlvalidate.validateFile(file.path);
-
-		if (!result.valid) {
-			const resultOutput = formatter(result);
-
-			this.emit(
-				'error',
-				new PluginError(pluginName, resultOutput, {
-					fileName: file.path,
-					showStack: false,
-				})
-			);
-		}
+		const result = htmlvalidate.validateString(
+			file.contents.toString(),
+			file.path
+		);
+		file.htmlValidateResult = result;
 
 		callback(null, file);
 	});
+}
+
+/**
+ * Print a report where all errors/warnings are listed.
+ *
+ * @param {String} formatterName Formatter supported by html-validate
+ *
+ * @return {Object} Stream object
+ */
+gulpHtmlValidate.format = (formatterName = defaultFormatter) => {
+	const formatter = htmlValidateCli.getFormatter(formatterName);
+
+	return processHtmlValidateResults((results) => {
+		const { errorCount, warningCount } = results;
+		if (errorCount === 0 && warningCount === 0) {
+			return;
+		}
+
+		const formatterOutput = formatter(results);
+		fancyLog(formatterOutput);
+	});
 };
+
+/**
+ * Fail when the stream ends if any error(s) occurred.
+ *
+ * @return {Object} Stream object
+ */
+gulpHtmlValidate.failAfterError = () => {
+	return processHtmlValidateResults((results) => {
+		const { errorCount } = results;
+
+		if (errorCount > 0) {
+			throw new PluginError(pluginName, {
+				message: `Failed with ${errorCount} ${
+					errorCount === 1 ? 'error' : 'errors'
+				}`,
+			});
+		}
+	});
+};
+
+/**
+ * Process html-validate reports with the given function.
+ *
+ * @param  {Function} action Callback which receives a list of results
+ *
+ * @return {Object} Stream object
+ */
+function processHtmlValidateResults(action) {
+	if (typeof action !== 'function') {
+		throw new TypeError(
+			`Invalid action: expected a function, got a ${typeof action}`
+		);
+	}
+
+	const results = [];
+
+	function onFile(file, encoding, callback) {
+		if (file.htmlValidateResult) {
+			const result = file.htmlValidateResult;
+			if (result) {
+				results.push(result);
+			}
+		}
+
+		callback(null, file);
+	}
+
+	function onStreamEnd(callback) {
+		const mergedResults = Reporter.merge(results);
+
+		tryResultAction(action, mergedResults, callback);
+	}
+
+	return through.obj(onFile, onStreamEnd);
+}
+
+function tryResultAction(action, result, done) {
+	try {
+		const isAsyncAction = action.length > 1;
+
+		if (isAsyncAction) {
+			action.call(this, result, done);
+		} else {
+			action.call(this, result);
+			done();
+		}
+	} catch (err) {
+		done(err || new Error('Unknown Error'));
+	}
+}
